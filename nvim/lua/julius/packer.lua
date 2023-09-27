08b7c04cc8824166fa25250cdb01d90708584a47
balljul <juliusball197@gmail.com>
2023-09-27 10:01:27 +0200

    ADDED nvim configs

---
+++ nvim/lua/julius/packer.lua
@@ -0,0 +1,78 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- set default theme (dark or light)
+-- By default the theme is define according to the preferences of the system
+-- let g:mkdp_theme = 'dark'
+-- set default theme (dark or light)
+-- By default the theme is define according to the preferences of the system
+-- let g:mkdp_theme = 'dark'
+ 
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    }
+    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use( 'nvim-treesitter/playground')
+    use( 'theprimeagen/harpoon' )
+    use( 'tpope/vim-fugitive') 
+
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    -- Lua
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    use {
+        'reline-nvim/feline.nvim',
+        require('feline').setup()
+    }
+end)
