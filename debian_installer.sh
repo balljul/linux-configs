0a5272144cdc33360b54ec8da779f303a5711f8c
balljul <juliusball197@gmail.com>
2023-11-28 23:06:12 +0100

    FINISHED the wm config

--- debian_installer.sh
+++ debian_installer.sh
@@ -9,6 +9,7 @@ fi
 
 # Global Vars
 wm_choice=""
+wm_config_choice=""
 snap_choice=""
 
 docker_choice=""
@@ -18,6 +19,8 @@ arduino_choice=""
 
 whatsapp_choice=""
 onenote_choice=""
+
+# Installer Functions
 
 check_choice() {
     local choice=$choice
@@ -42,78 +45,92 @@ check_choice() {
 
 }
 
-# Installer Scripts
-
 install_wm() {
-    clear  
-
+    clear -x
     echo "Installing the Window Manager: "
-
     # sudo apt install bspwm -y &> /dev/null
     echo -e "Bspwm \xE2\x9C\x94"  
-
     # sudo apt install sxhkd -y &> /dev/null
     echo -e "Sxhkd \xE2\x9C\x94"  
-
     # sudo apt install polybar -y &> /dev/null
     echo -e "Polybar \xE2\x9C\x94"  
-
     # sudo apt install compton -y &> /dev/null
     echo -e "Compton \xE2\x9C\x94"  
-
     # sudo apt install rofi -y &> /dev/null
     echo -e "Rofi \xE2\x9C\x94"  
-
     # sudo apt install dunst -y &> /dev/null
     echo -e "Dunst \xE2\x9C\x94"  
-
     # sudo apt install nitrogen -y &> /dev/null
     echo -e "Nitrogen \xE2\x9C\x94"  
-
     # sudo apt install i3lock -y &> /dev/null
     echo -e "i3lock \xE2\x9C\x94"  
-
     # sudo apt install redshift -y &> /dev/null
     echo -e "Redshift \xE2\x9C\x94"  
-
     # sudo apt install cmus -y &> /dev/null
     echo -e "Cmus \xE2\x9C\x94"  
-
     # sudo apt install ranger -y &> /dev/null
     echo -e "Ranger \xE2\x9C\x94"  
+    # sudo apt install picom -y &> /dev/null
+    echo -e "Picom \xE2\x9C\x94"  
 
+    while true; do
+        read -p "Do you want to load my configs for the WM: " choice
+        wm_config_choice=$(check_choice "$choice")
+
+        if [ "$wm_config_choice" -eq 1 ]; then
+            choice=""
+            configure_wm
+            break
+        elif [ "$wm_config_choice" -eq 0 ]; then
+            choice=""
+            break
+        elif [ "$wm_config_choice" -eq 2 ]; then
+            choice=""
+            continue
+        else
+            choice=""
+            continue
+        fi
+    done
 }
 
-# Installation Choices
+configure_wm() {
+    clear -x
+    echo "Configuring the Window Manager: "
+    # cp -r ./.config/bspwm ~/.config/
+    echo -e "Bspwm config loaded \xE2\x9C\x94"  
+    # cp -r ./.config/sxhkd/ ~/.config/
+    echo -e "Sxhkd config loaded \xE2\x9C\x94"  
+    # cp -r ./.config/polybar/ ~/.config/
+    echo -e "Polybar config loaded \xE2\x9C\x94"  
+    # cp -r ./.config/rofi/ ~/.config/
+    echo -e "Rofi config loaded \xE2\x9C\x94"  
+    # cp -r ./.config/picom/ ~/.config/
+    echo -e "Picom config loaded \xE2\x9C\x94"  
+}
+
+
+
+
+
+
+## Install wm
 while true; do
     read -p "Do you want to start the WM Installation: " choice
-    wm_choice=$(check_choice "$wm_choice")
+    wm_choice=$(check_choice "$choice")
 
     if [ "$wm_choice" -eq 1 ]; then
+        choice=""
         install_wm
         break
     elif [ "$wm_choice" -eq 0 ]; then
+        choice=""
         break
     elif [ "$wm_choice" -eq 2 ]; then
+        choice=""
         continue
     else
+        choice=""
         continue
     fi
 done
-
-while true; do
-    read -p "Do you want to load my configs for the WM: " choice
-    wm_choice=$(check_choice "$wm_choice")
-
-    if [ "$wm_choice" -eq 1 ]; then
-        configure_wm
-        break
-    elif [ "$wm_choice" -eq 0 ]; then
-        break
-    elif [ "$wm_choice" -eq 2 ]; then
-        continue
-    else
-        continue
-    fi
-done
-
