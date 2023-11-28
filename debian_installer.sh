28f78e48dbd296c3c8ea7775635be8ded821e425
balljul <juliusball197@gmail.com>
2023-11-28 23:16:21 +0100

    ADDED snap and xfce4 into my installer file

--- debian_installer.sh
+++ debian_installer.sh
@@ -11,6 +11,7 @@ fi
 wm_choice=""
 wm_config_choice=""
 snap_choice=""
+xfce_choice=""
 
 docker_choice=""
 npm_choice=""
@@ -46,7 +47,6 @@ check_choice() {
 }
 
 install_wm() {
-    clear -x
     echo "Installing the Window Manager: "
     # sudo apt install bspwm -y &> /dev/null
     echo -e "Bspwm \xE2\x9C\x94"  
@@ -95,7 +95,6 @@ install_wm() {
 }
 
 configure_wm() {
-    clear -x
     echo "Configuring the Window Manager: "
     # cp -r ./.config/bspwm ~/.config/
     echo -e "Bspwm config loaded \xE2\x9C\x94"  
@@ -109,10 +108,40 @@ configure_wm() {
     echo -e "Picom config loaded \xE2\x9C\x94"  
 }
 
+install_snap() {
+    sudo apt install snap -y &> /dev/null
+    echo -e "Snapstore installed \xE2\x9C\x94"  
+}
 
+install_xfce() {
+    sudo apt install xfce4-terminal -y &> /dev/null
+    echo -e "XFCE4 installed \xE2\x9C\x94"  
 
+    while true; do
+        read -p "Do you want to load my configs for the terminal: " choice
+        xfce_choice=$(check_choice "$choice")
 
+        if [ "$xfce_choice" -eq 1 ]; then
+            choice=""
+            configure_xfce
+            break
+        elif [ "$xfce_choice" -eq 0 ]; then
+            choice=""
+            break
+        elif [ "$xfce_choice" -eq 2 ]; then
+            choice=""
+            continue
+        else
+            choice=""
+            continue
+        fi
+    done
+}
+configure_xfce() {
+    # cp -r ./.config/xfce4 ~/.config
+    echo -e "XFCE4 configured \xE2\x9C\x94"  
 
+}
 
 ## Install wm
 while true; do
@@ -127,6 +156,46 @@ while true; do
         choice=""
         break
     elif [ "$wm_choice" -eq 2 ]; then
+        choice=""
+        continue
+    else
+        choice=""
+        continue
+    fi
+done
+
+while true; do
+    read -p "Do you want to install snap: " choice
+    snap_choice=$(check_choice "$choice")
+
+    if [ "$snap_choice" -eq 1 ]; then
+        choice=""
+        install_snap
+        break
+    elif [ "$snap_choice" -eq 0 ]; then
+        choice=""
+        break
+    elif [ "$snap_choice" -eq 2 ]; then
+        choice=""
+        continue
+    else
+        choice=""
+        continue
+    fi
+done
+
+while true; do
+    read -p "Do you want to install the xfce4 terminal: " choice
+    xfce_choice=$(check_choice "$choice")
+
+    if [ "$xfce_choice" -eq 1 ]; then
+        choice=""
+        install_xfce
+        break
+    elif [ "$xfce_choice" -eq 0 ]; then
+        choice=""
+        break
+    elif [ "$xfce_choice" -eq 2 ]; then
         choice=""
         continue
     else
