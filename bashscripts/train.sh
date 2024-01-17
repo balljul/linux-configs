2ffcbd778109c2908b71c2069d236c17c043f3b9
balljul <juliusball197@gmail.com>
2024-01-17 08:49:45 +0100

    UPDATED train script

--- bashscripts/train.sh
+++ bashscripts/train.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+PACKAGE_NAME="sl"
+
+if dpkg-query -W -f='${Status}' $PACKAGE_NAME 2>/dev/null | grep -q "ok installed"; then
+    echo "The Package $PACKAGE_NAME is installed"
+else 
+    echo "Please install the following package: $PACKAGE_NAME"
+    exit 1
+fi
+
 while true; do
     sl
 done
