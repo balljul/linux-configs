d0af50a846b07e99e507fccaab8d1c13c3ad708c
balljul <juliusball197@gmail.com>
2024-02-07 20:44:48 +0100

    ADDED remap

--- .config/nvim/lua/julius/remap.lua
+++ .config/nvim/lua/julius/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader =  " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>rr", [[:RustRun<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>w", "<C-w>",{ noremap = false, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>i", "<C-a>",{ noremap = false, silent = true })
